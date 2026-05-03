b7eeb879411523e990328bd4cc034098089410d9
TejaswiniSajja <tejaswinisajja25@gmail.com>
2026-05-03 22:57:14 +0530

    Cleaned ignored files

--- utils.py
+++
@@ -1,8 +0,0 @@
-def safe_get(data, *keys, default=None):
-    current = data
-    for key in keys:
-        if isinstance(current, dict):
-            current = current.get(key, default)
-        else:
-            return default
-    return current
